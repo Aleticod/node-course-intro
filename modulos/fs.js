39ec4c8d9c7c69be1842cc9cf024492a3f3bf55c
Aleticod <193837@unsaac.edu.pe>
2023-08-28 17:34:16 -0500

    :sparkles: Add file system management

---
+++ modulos/fs.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+function leer(ruta, callback) {
+    fs.readFile(ruta, (err, data) => {
+        callback(data.toString());
+    })
+}
+
+function escribir(ruta, contenido, callback) {
+    fs.writeFile(ruta, contenido, function(err) {
+        if (err) {
+            console.error('No he podido escribirlo');
+        } else {
+            console.log('Se ha escrito correctamente')
+        }
+    })
+}
+
+function borrar(ruta, callback) {
+    fs.unlink(ruta, callback);
+}
+
+// escribir(__dirname + '/archivo1.txt', 'Soy un archivo nuevo', console.log )
+
+// leer(__dirname + '/archivo1.txt', console.log)
+
+borrar(__dirname + '/archivo1.txt', console.log)
